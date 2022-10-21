3eeffb79088eb185c1a7356727da4b86bad4166b
DennnnyH <97718766+DennnnyH@users.noreply.github.com>
2022-10-20 20:37:06 -1000

    JS for buttons

--- routes/edit.js
+++ routes/edit.js
@@ -1,3 +1,12 @@
+const edit = document.getElementById("edit questions");
+const save = document.getElementById("btn save");
+const add = document.getElementById("btn new questions");
+const form = document.getElementById("originalFormQs");
+edit.addEventListener("click",function(){
+    form.contentEditable = true;
+
+});
+
 const express = require('express');
 const router = express.Router();
 
