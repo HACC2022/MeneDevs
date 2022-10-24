5cd87b1a00b8ee28c760f9ec460edce58d5dac04
JT <64624898+BannaDanna@users.noreply.github.com>
2022-10-23 21:23:34 -1000

    Added and linked login.js

---
+++ public/javscripts/login.js
@@ -0,0 +1,12 @@
+const getCreds = document.getElementById("login");
+
+getCreds.addEventListener("click", function()
+{
+    const email = document.getElementById("email");
+    const pass = document.getElementById("pass");
+
+    if(email == null || pass == null)
+    {
+        return;
+    }
+})
